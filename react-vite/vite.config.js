666468c80e08c1b8c9877c27a89ba832a2c8f4aa
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-09-02 18:33:06 -0700

    worked with vite to try and get build chunks to be smaller

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -3,57 +3,33 @@ import eslintPlugin from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vitejs.dev/config/
-// export default defineConfig((mode) => ({
-//   plugins: [
-//     react(),
-//     eslintPlugin({
-//       lintOnStart: true,
-//       failOnError: mode === "production",
-//     }),
-//     // visualizer({ open: true })
-//   ],
-//   build: {
-//     rollupOptions: {
-//       plugins: [
-//         visualizer({
-//           filename: "./dist/stats.html",
-//           open: true,
-//         }),
-//       ],
-//       output: {
-//         manualChunks: {
-//           vendor: ["react", "react-dom", "react-icons"],
-//         },
-//       },
-//     },
-//   },
-//   server: {
-//     open: true,
-//     proxy: {
-//       "/api": "http://127.0.0.1:8000",
-//     },
-//   },
-// }));
-export default defineConfig({
+// // https://vitejs.dev/config/
+export default defineConfig((mode) => ({
   plugins: [
     react(),
     eslintPlugin({
       lintOnStart: true,
-      failOnError: process.env.NODE_ENV === "production",
+      failOnError: mode === "production",
+    }),
+    visualizer({
+      filename: './dist/stats.html',
+      open: true,
     }),
   ],
   build: {
     rollupOptions: {
       plugins: [
         visualizer({
-          filename: './dist/stats.html',
+          filename: "./dist/stats.html",
           open: true,
         }),
       ],
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-icons'],
+          'react-vendor': ['react', 'react-dom'],
+          'redux-vendor': ['redux', 'react-redux'],
+          'date-utils': ['date-fns'],
+          'react-icons': ['react-icons/fa6'],
         },
       },
     },
@@ -64,4 +40,65 @@ export default defineConfig({
       "/api": "http://127.0.0.1:8000",
     },
   },
-});
+}));
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     eslintPlugin({
+//       lintOnStart: true,
+//       failOnError: process.env.NODE_ENV === "production",
+//     }),
+//   ],
+//   build: {
+//     rollupOptions: {
+//       plugins: [
+//         visualizer({
+//           filename: './dist/stats.html',
+//           open: true,
+//         }),
+//       ],
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-dom', 'react-icons'],
+//         },
+//       },
+//     },
+//   },
+//   server: {
+//     open: true,
+//     proxy: {
+//       "/api": "http://127.0.0.1:8000",
+//     },
+//   },
+// });
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     eslintPlugin({
+//       lintOnStart: true,
+//       failOnError: process.env.NODE_ENV === "production",
+//     }),
+//   ],
+//   build: {
+//     rollupOptions: {
+//       plugins: [
+//         visualizer({
+//           filename: './dist/stats.html',
+//           open: true,
+//         }),
+//       ],
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-dom'],
+//         },
+//       },
+//     },
+//   },
+//   server: {
+//     open: true,
+//     proxy: {
+//       "/api": "http://127.0.0.1:8000",
+//     },
+//   },
+// });
