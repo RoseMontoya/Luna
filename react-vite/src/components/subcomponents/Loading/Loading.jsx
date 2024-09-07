e1c723834678880042a3fc8952b606ae337c3b36
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-09-07 16:14:47 -0700

    ready to render

--- react-vite/src/components/subcomponents/Loading/Loading.jsx
+++ react-vite/src/components/subcomponents/Loading/Loading.jsx
@@ -1,6 +1,12 @@
+import { useNav } from "../../../context/navContext";
+
 function Loading() {
+    const { navOpen } = useNav();
+
     return (
-        <h1>Loading</h1>
+        <main className={`${navOpen ? "nav-open" : ""} side-nav`}>
+            <h1>Loading</h1>
+        </main>
     )
 }
 
