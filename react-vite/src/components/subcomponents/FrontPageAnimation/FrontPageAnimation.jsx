45f486519dab48345a4ed026c53a05f77c6023ac
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-09-07 16:09:08 -0700

    fixing images for landing page

--- react-vite/src/components/subcomponents/FrontPageAnimation/FrontPageAnimation.jsx
+++ react-vite/src/components/subcomponents/FrontPageAnimation/FrontPageAnimation.jsx
@@ -7,16 +7,19 @@ function FrontPageAnimation() {
 
     return (
         <div id='moon-cycle'>
-            <img src="../../public/images/moon/full-moon.png" id="full" />
-            <img src="../../public/images/moon/waning-sliver.png" id='waning-sliver'/>
-            <img src="../../public/images/moon/waning-gibbous.png" id='waning-gibbous'/>
-            <img src="../../public/images/moon/waning-half.png" id='waning-half'/>
-            <img src="../../public/images/moon/waning-cres.png" id="waning-cres"/>
-            <img src="../../public/images/moon/new-moon.png" id="new"/>
-            <img src="../../public/images/moon/waxing-sliver.png" id="waxing-sliver" />
-            <img src="../../public/images/moon/waxing-cres.png" id="waxing-cres" />
-            <img src="../../public/images/moon/waxing-half.png" id="waxing-half"/>
-            <img src="../../public/images/moon/waxing-gibbous.png" id="waxing-gibbous"/>
+            <div id="moons">
+                <img src="/images/moon/full-moon.png" id="full" />
+                {/* <div id="full"></div> */}
+                <img src="/images/moon/waning-sliver.png" id='waning-sliver'/>
+                <img src="/images/moon/waning-gibbous.png" id='waning-gibbous'/>
+                <img src="/images/moon/waning-half.png" id='waning-half'/>
+                <img src="/images/moon/waning-cres.png" id="waning-cres"/>
+                <img src="/images/moon/new-moon.png" id="new"/>
+                <img src="/images/moon/waxing-sliver.png" id="waxing-sliver" />
+                <img src="/images/moon/waxing-cres.png" id="waxing-cres" />
+                <img src="/images/moon/waxing-half.png" id="waxing-half"/>
+                <img src="/images/moon/waxing-gibbous.png" id="waxing-gibbous"/>
+            </div>
             <div id="polar-star">
                 <GiPolarStar />
             </div>
