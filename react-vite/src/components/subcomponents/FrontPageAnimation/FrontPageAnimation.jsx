199d99594a5a5de85e1dc51e241bfcb644f423c4
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-09-06 20:31:53 -0700

    working on front page component

--- react-vite/src/components/subcomponents/FrontPageAnimation/FrontPageAnimation.jsx
+++ react-vite/src/components/subcomponents/FrontPageAnimation/FrontPageAnimation.jsx
@@ -4,15 +4,16 @@ function FrontPageAnimation() {
 
     return (
         <div id='moon-cycle'>
-            <img src="../../public/images/moon/full.png" id="full" />
+            <img src="../../public/images/moon/full-moon.png" id="full" />
+            <img src="../../public/images/moon/waning-sliver.png" id='waning-sliver'/>
+            <img src="../../public/images/moon/waning-gibbous.png" id='waning-gibbous'/>
             <img src="../../public/images/moon/waning-half.png" id='waning-half'/>
             <img src="../../public/images/moon/waning-cres.png" id="waning-cres"/>
-            <img src="../../public/images/moon/waning-sliver.png" id='waning-sliver'/>
-            {/* <img src="../../public/images/moon/full.png" id="new"/> */}
-            <div id='new'></div>
+            <img src="../../public/images/moon/new-moon.png" id="new"/>
             <img src="../../public/images/moon/waxing-sliver.png" id="waxing-sliver" />
             <img src="../../public/images/moon/waxing-cres.png" id="waxing-cres" />
             <img src="../../public/images/moon/waxing-half.png" id="waxing-half"/>
+            <img src="../../public/images/moon/waxing-gibbous.png" id="waxing-gibbous"/>
         </div>
     )
 
